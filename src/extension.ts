04c9d90e96da91b952d65b2719275cb72a66097f
Rene Saarsoo <nene@triin.net>
2026-06-21 15:02:14 +0300

    Add "|" to where user clicked in editor

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,14 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("sql-insert-editor.deleteColumn", () => {
-      vscode.window.showInformationMessage(
-        "Delete this column (not yet implemented)",
-      );
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      const position = editor.selection.active;
+      editor.edit((editBuilder) => {
+        editBuilder.insert(position, "|");
+      });
     }),
   );
 }
