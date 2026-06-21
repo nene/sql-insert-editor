2fca780f6548a18db8fd2cdb886e51ba78059112
Rene Saarsoo <nene@triin.net>
2026-06-21 18:54:05 +0300

    Improved test data

--- test-data/test.sql
+++ test-data/test.sql
@@ -1,2 +1,10 @@
-INSERT INTO my_table (column1, column2)
-VALUES ('value1', 'value2'), ('value3', 'value4'), ('value5', 'value6');
+INSERT INTO product (id, name, category, price, stock, manufacturer)
+VALUES
+  (1, 'Batman 12" Figure', 'DC Comics', 29.99, 150, 'McFarlane Toys'),
+  (2, 'Spider-Man Action Figure', 'Marvel', 24.99, 200, 'Hasbro'),
+  (3, 'He-Man Classic', 'Masters of the Universe', 19.99, 75, 'Mattel'),
+  (4, 'Optimus Prime Deluxe', 'Transformers', 49.99, 50, 'Hasbro'),
+  (5, 'Teenage Mutant Ninja Turtle - Leonardo', 'TMNT', 14.99, 300, 'Playmates'),
+  (6, 'Darth Vader 6" Black Series', 'Star Wars', 34.99, 120, 'Hasbro'),
+  (7, 'Goku Ultra Instinct', 'Dragon Ball Z', 27.99, 90, 'Bandai'),
+  (8, 'Wonder Woman Gold Armor', 'DC Comics', 32.99, 60, 'McFarlane Toys');
