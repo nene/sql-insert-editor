caf533a44ac71c269ecbe7938761b77446823958
Rene Saarsoo <nene@triin.net>
2026-06-21 14:50:11 +0300

    Edd context-menu command "Delete this column"

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,12 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("sql-insert-editor is now active");
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("sql-insert-editor.deleteColumn", () => {
+      vscode.window.showInformationMessage("Delete this column (not yet implemented)");
+    })
+  );
 }
 
 export function deactivate() {}
